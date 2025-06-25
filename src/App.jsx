94dfd7921ca03235e1ecc5a4bced4bf7ff9f74f6
Victoria846 <maria.morales@anima.edu.uy>
2025-06-24 23:47:01 -0300

    update
    bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import "./app.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Moviecontainer from "./components/Moviecontainer";
 
 function App() {
   return (
-    <div className="container mt-4">
-      <h1 className=" mb-4 ">Hackflix</h1>
-      <p>bfhkbdlikjnwedbksjn</p>
+    <div className="container mt-4 min-vh-100 ">
+      <h1 className=" mb-4 text-white ">Hackflix</h1>
+      <p className="parrafo">
+        Encuentra títulos originales que no hay en ningún otro streamming.
+        Películas, series, especiales.. Y todos los elegidos, solo aquí en
+        Hackflix.
+      </p>
       <Moviecontainer />
     </div>
   );
