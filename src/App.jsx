8c3c209ee29fa5b036c27c5fcdb7f1510b43265f
roblesregina <regina.robles@anima.edu.uy>
2025-06-25 19:55:13 -0300

    UPDATE
    Agregué el react dom para las páginas emergentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Moviecontainer from "./components/Moviecontainer";
+import MovieDetail from "./components/MovieDetail";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="container mt-4">
       <h1>🎬 Hackflix</h1>
       <Moviecontainer />
+      <Routes>
+        <Route path="/" element={<Moviecontainer />} />
+        <Route path="/pelicula/:id" element={<MovieDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Movies = ({ movies }) => {
   if (!movies || movies.length === 0) {
@@ -9,19 +10,23 @@ const Movies = ({ movies }) => {
     <div className="row">
       {movies.map((movie) => (
         <div key={movie.id} className="col-md-4 mb-4">
-          <div className="card h-100">
-            <img
-              src={movie.poster_path}
-              alt={movie.title}
-              className="card-img-top"
-              style={{ height: "400px", objectFit: "cover" }}
-            />
-            <div className="card-body d-flex flex-column">
-              <h5 className="card-title">{movie.title}</h5>
-              <p className="card-text flex-grow-1">{movie.overview}</p>
-              {/* Aquí podés integrar el componente Rating para las estrellas */}
+          <Link
+            to={`/pelicula/${movie.id}`}
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <div className="card h-100">
+              <img
+                src={movie.poster_path}
+                alt={movie.title}
+                className="card-img-top"
+                style={{ height: "400px", objectFit: "cover" }}
+              />
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title">{movie.title}</h5>
+                <p className="card-text flex-grow-1">{movie.overview}</p>
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       ))}
     </div>
